fc85dfc69f4d08b88762431544c123b8bea9afc9
Kai Sassnowski <ksassnowski@gmail.com>
2017-02-21 10:04:56 +0100

    Add example script

--- code/bin/container.php
+++ code/bin/container.php
@@ -1,3 +1,15 @@
 <?php
 
 require __DIR__.'/../vendor/autoload.php';
+
+use Container\Logging\DatabaseLogger;
+use Container\Models\User;
+use Container\Session\PhpSessionStorage;
+
+$logger = new DatabaseLogger();
+$storage = new PhpSessionStorage($logger);
+$user = new User($storage);
+
+/** @var User $user */
+$user->setLanguage('de');
+echo $user->getLanguage('de');
